688b1ad4d2971f333720b0f3b5c738613a86bfe8
Andre-kodehode <AAA03022025@gmail.com>
2025-04-24 17:08:33 +0200

    Started gallery

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const wheel = document.getElementById("wheel");
-  if (wheel) {
-    wheel.addEventListener("wheel", (e) => e.preventDefault());
-  }
-});
